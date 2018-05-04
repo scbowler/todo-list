26321c7e971a2cfd8996b37d75f2d9b24f447ab0
Scott Bowler <scott.bowler@learningfuze.com>
2018-05-04 16:43:23 -0700

    Added delete buttons

--- src/components/add-items.js
+++ src/components/add-items.js
@@ -15,6 +15,11 @@ class AddItem extends Component {
         console.log('Item:', this.state);
 
         this.props.add(this.state);
+
+        this.setState({
+            title: '',
+            details: ''
+        });
     }
 
     render(){
